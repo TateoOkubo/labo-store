d214b8f63a9d4e67b1d0ad4313b00d4549261690
Tateo Okubo <neteooo@gmail.com>
2017-01-24 12:52:00 +0000

    add item/type model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "seeds runninng..."
+
+# ユーザー
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password_digest: "foobar",
+             admin: true)
+print "."
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name: name,
+              email: email,
+              password_digest: password,
+              admin: false)
+  print "."
+end
+puts "\n-> User_seeds OK!!"
+
+ItemClass.create(class_name: "Ice Cream")
+ItemClass.create(class_name: "Cup Noodle")
+puts "ItemClass_seeds OK"
+
+#10.times do |n|
+#  name = Faker::Food.ingredient
+#  price = Faker::Number.number(4)
+  
+  #Item.create!(name: name, price: price)
+#end
+
