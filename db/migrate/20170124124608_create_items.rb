0edff1734bcbbd5c35dd841f824fa54153a25261
Tateo Okubo <neteooo@gmail.com>
2017-03-02 13:58:29 +0000

    delete foreign_key

--- db/migrate/20170124124608_create_items.rb
+++ db/migrate/20170124124608_create_items.rb
@@ -1,7 +1,7 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
-      t.references :type, index: true, foreign_key: true
+      t.references :type, index: true#, foreign_key: true
       t.string :name
       t.integer :price
 
