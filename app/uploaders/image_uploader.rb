95a9074b0c5ec628404e68d23d6bd1655dd0c392
Tateo Okubo <neteooo@gmail.com>
2017-03-11 13:26:40 +0000

    fix image_uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -20,12 +20,22 @@ class ImageUploader < CarrierWave::Uploader::Base
   #  "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   #end
 
+  #def filename
+  #  "#{SecureRandom.uuid}.#{file.extension}" if original_filename.present?
+  #end
   def filename
-    "#{SecureRandom.uuid}.#{file.extension}" if original_filename.present?
+   "#{secure_token}.#{file.extension}" if original_filename.present?
   end
   
   def public_id
     model.id
+  end
+  
+  protected
+  
+  def secure_token
+    var = :"@#{mounted_as}_secure_token"
+    model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
   end
 
 end
